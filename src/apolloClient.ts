73a1133e466a8b400de039458811821213aff594
Ahmed Alatawi <aalat001@gmail.com>
2024-09-10 01:02:37 -0400

    Added error alert

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -4,15 +4,17 @@ import { onError } from '@apollo/client/link/error'
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.forEach(({ message, locations, path }) => {
-      console.error(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
+      const error = `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      alert(error)
+      console.error(error)
       if (message === 'Not Authorized') {
         console.error('Not Authorized')
       }
     })
   if (networkError) {
-    console.error(`[Network error]: ${networkError}`)
+    const error = `[Network error]: ${networkError}`
+    alert(error)
+    console.error(error)
   }
 })
 
